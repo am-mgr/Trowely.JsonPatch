4f655e42b30c758eaaa90cadf7fc4e14bf4fd96b
am-mgr <am.mgr.dev@gmail.com>
2018-10-28 17:22:36 +0530

    Initial Commit

---
+++ src/Trowely.JsonPatch/Library.fs
@@ -0,0 +1,5 @@
+namespace Trowely.JsonPatch
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ test/Trowely.JsonPatch.FSharp.Tests/Library.fs
@@ -0,0 +1,5 @@
+namespace Trowely.JsonPatch.FSharp.Tests
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
